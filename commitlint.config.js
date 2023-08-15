9247529a3faa684dc016c9793fe5ee75e9aab014
Steve Dodier-Lazaro <steve.dodier-lazaro@backmarket.com>
2023-08-15 11:07:12 +0200

    chore: Add husky and commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [2, 'never'],
+    'subject-case': [2, 'always', ['sentence-case']],
+    'type-enum': [2, 'always', [
+      'feat',
+      'fix',
+      'docs',
+      'refactor',
+      'style',
+      'test',
+      'revert',
+      'chore',
+    ]],
+  },
+}
