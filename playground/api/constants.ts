81024c6a3dc7b9a50c2a4a04e10de77d6eb1124b
Steve Dodier-Lazaro <steve.dodier-lazaro@backmarket.com>
2023-08-16 08:17:38 +0200

    fix: Remove playground for now

--- playground/api/constants.ts
+++
@@ -1,5 +0,0 @@
-import path from 'path'
-
-export const API_PORT = process.env.API_PORT || process.env.PORT || 3002
-export const ROOT_DIR = path.resolve(__dirname, '../..')
-export const TRANS_DIR = path.resolve(ROOT_DIR, 'transformations')
