611340cc40ea9c5b96e911d4004248c26f0b3aba
Steve Dodier-Lazaro <steve.dodier-lazaro@backmarket.com>
2023-08-17 23:03:36 +0200

    chore: Lint CJS files on commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,4 @@
 export default {
-  '*.{js,jsx,ts,tsx}': ['yarn lint:eslint:staged', 'yarn format:staged'],
+  '*.{js,jsx,ts,tsx,cjs}': ['yarn lint:eslint:staged', 'yarn format:staged'],
   '*.{json,md,scss,css,html,yml}': ['yarn format:fix'],
 }
