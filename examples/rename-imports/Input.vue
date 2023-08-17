1989328ec16a9ff7c650ccf8cb3eff973616898d
Steve Dodier-Lazaro <steve.dodier-lazaro@backmarket.com>
2023-08-17 07:46:46 +0200

    style: Apply formatting to example inputs

--- examples/rename-imports/Input.vue
+++ examples/rename-imports/Input.vue
@@ -2,14 +2,14 @@
   <div class="flex-1 min-h-4 w-6">
     <blockquote class="p-[2.5rem]">
       <IconQuote size="medium" />
-and when we speak we are afraid
-our words will not be heard
-nor welcomed
-but when we are silent
-we are still afraid
-So it is better to speak
-remembering
-we were never meant to survive
+      and when we speak we are afraid<br />
+      our words will not be heard<br />
+      nor welcomed<br />
+      but when we are silent<br />
+      we are still afraid<br />
+      So it is better to speak<br />
+      remembering<br />
+      we were never meant to survive
     </blockquote>
     <footer>― Audre Lorde, <cite>The Black Unicorn: Poems</cite></footer>
     <Button>Share poem</Button>
@@ -17,7 +17,7 @@ we were never meant to survive
 </template>
 
 <script lang="ts">
-import { Button, IconQuote } from "@orgname/old-package";
+import { Button, IconQuote } from '@orgname/old-package'
 export default {
   components: {
     Button,
