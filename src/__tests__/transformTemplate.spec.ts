caf3ee79ce89df998f5ca4fbf3cd1b2bb4f5f48f
Steve Dodier-Lazaro <steve.dodier-lazaro@backmarket.com>
2023-08-20 10:19:56 +0200

    test: Add test files to implement later

---
+++ src/__tests__/transformTemplate.spec.ts
@@ -0,0 +1,10 @@
+describe('transform', () => {
+  describe('template', () => {
+    test.todo('everything')
+    // Test outcome with uncompilable template
+    // Test identity transform, check dtor identical + return false
+    // Test a transform (nothing exhaustive, that'll be API tests), check dtor modified + return true
+    // Text node in SSR
+    // Text call node in CSR
+  })
+})
